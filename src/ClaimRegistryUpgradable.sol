a0c52a63012ab305df3bcfb32cb40a38cc2a42c7
4rgon4ut <egorlaptev98@gmail.com>
2024-03-22 15:25:09 +0000

    docs: add registry documentation

--- src/ClaimRegistryUpgradable.sol
+++ src/ClaimRegistryUpgradable.sol
@@ -8,12 +8,24 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
+/**
+ * @title ClaimRegistryUpgradable
+ * @dev A contract for managing claim registrations and withdrawals with upgradability features.
+ */
 contract ClaimRegistryUpgradable is
     IClaimRegistryUpgradable,
     UUPSUpgradeable,
     OwnableUpgradeable,
     PausableUpgradeable
 {
+    // State variables
+
+    // Public variables
+
+    // Events
+
+    // Modifiers
+
     //uint256 public threshold;
     ISBCDepositContract public depositContract;
     mapping(address => Config) public configs;
@@ -54,13 +66,21 @@ contract ClaimRegistryUpgradable is
         _;
     }
 
+    // Constructor
+
+    /**
+     * @dev Disable implementaton contract initializers
+     */
     constructor() {
         _disableInitializers();
     }
 
-    function _authorizeUpgrade(address) internal override onlyOwner {}
+    // Proxy initializing and upgrading
 
-    // TODO: recheck
+    /**
+     * @dev Initializes the proxy contract, intended to be called only once.
+     * @param _depositContract Address of the deposit contract.
+     */
     function initialize(address _depositContract) public initializer {
         __Ownable_init(msg.sender);
         __Pausable_init();
@@ -69,10 +89,94 @@ contract ClaimRegistryUpgradable is
         depositContract = ISBCDepositContract(_depositContract);
     }
 
+    /**
+     * @dev Ensures that only owner can upgrade the implementation.
+     * @param newImplementation Address of the new implementation.
+     */
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    // External functions
+
+    /**
+     * @dev Resolves the claimable addresses and returns flag and calldata.
+     * @return flag Boolean flag indicating whether claimable addresses exist.
+     * @return cdata ABI-encoded calldata for batch claiming.
+     */
+    function resolve() external view returns (bool flag, bytes memory cdata) {
+        address[] memory addresses = getClaimableAddresses();
+        if (addresses.length == 0) {
+            return (false, "");
+        }
+        return (true, abi.encodeWithSelector(this.claimBatch.selector, addresses));
+    }
+
+    /**
+     * @dev Gets the claimable addresses based on configured thresholds.
+     * @return claimableAddresses Array of claimable addresses.
+     * @todo Consider offset shifting option for huge validators set.
+     */
+    function getClaimableAddresses() public view returns (address[] memory) {
+        address[] memory claimableAddresses = new address[](validators.length);
+        uint256 counter = 0;
+
+        for (uint256 i = 0; i < validators.length; i++) {
+            address val = validators[i];
+            // skip for inactive configs and zero withdrawable amount
+            uint256 withdrawableAmount = depositContract.withdrawableAmount(val);
+            if (withdrawableAmount == 0 || configs[val].status == ConfigStatus.INACTIVE) {
+                continue;
+            }
+            // add address to list if amount or time condition met
+            if (withdrawableAmount > configs[val].amountThreshold) {
+                claimableAddresses[counter] = val;
+                counter++;
+            } else if (
+                configs[val].timeThreshold > 0 && block.timestamp - configs[val].lastClaim > configs[val].timeThreshold
+            ) {
+                claimableAddresses[counter] = val;
+                counter++;
+            }
+        }
+
+        if counter != validators.length {
+            // as we used new address[](validators.length); we have to trim array to remove zero addresses
+            // since we can't resize array in memory, we need to create new one where we will copy all non-zero addresses
+            address[] memory trimmedClaimableAddresses = new address[](counter);
+            for (uint256 i = 0; i < claimableAddresses.length; i++) {
+                if (claimableAddresses[i] != address(0)) {
+                    trimmedClaimableAddresses[i] = claimableAddresses[i];
+                }
+            }
+            return trimmedClaimableAddresses;
+        }
+        return claimableAddresses;
+    }
+
+    // Public functions
+
+    /**
+     * @dev Gets the length of the validators array.
+     * @return Length of the validators array.
+     */
     function getValidatorsLength() public view returns (uint256) {
         return validators.length;
     }
 
+    /**
+     * @dev Checks if a configuration is active for a withdrawal address.
+     * @param _withdrawalAddress The withdrawal address to check.
+     * @return Boolean indicating whether the configuration is active.
+     */
+    function isConfigActive(address _withdrawalAddress) public view returns (bool) {
+        return configs[_withdrawalAddress].status == ConfigStatus.ACTIVE;
+    }
+
+    /**
+     * @dev Registers a user with withdrawal credentials.
+     * @param _withdrawalAddress The address to register for withdrawals.
+     * @param _timeThreshold Time threshold for withdrawal.
+     * @param _amountThreshold Amount threshold for withdrawal.
+     */
     function register(address _withdrawalAddress, uint256 _timeThreshold, uint256 _amountThreshold)
         public
         nonZeroParams(_timeThreshold, _amountThreshold)
@@ -83,19 +187,12 @@ contract ClaimRegistryUpgradable is
         emit Register(msg.sender);
     }
 
-    // TODO: consider out of gas
-    function claimBatch(address[] calldata withdrawalAddresses) public {
-        for (uint256 i = 0; i < withdrawalAddresses.length; i++) {
-            claim(withdrawalAddresses[i]);
-        }
-        emit ClaimBatch(msg.sender, withdrawalAddresses);
-    }
-
-    function claim(address withdrawalAddress) public {
-        depositContract.claimWithdrawal(withdrawalAddress);
-        configs[withdrawalAddress].lastClaim = block.timestamp;
-    }
-
+    /**
+     * @dev Updates the configuration for a withdrawal address.
+     * @param _withdrawalAddress The withdrawal address to update configuration for.
+     * @param _timeThreshold New time threshold for withdrawal.
+     * @param _amountThreshold New amount threshold for withdrawal.
+     */
     function updateConfig(address _withdrawalAddress, uint256 _timeThreshold, uint256 _amountThreshold)
         public
         nonZeroParams(_timeThreshold, _amountThreshold)
@@ -112,6 +209,10 @@ contract ClaimRegistryUpgradable is
         _setConfig(_timeThreshold, _amountThreshold);
     }
 
+    /**
+     * @dev Unregisters a withdrawal address.
+     * @param _withdrawalAddress The withdrawal address to unregister.
+     */
     function unregister(address _withdrawalAddress)
         public
         ownerOrAdmin(_withdrawalAddress)
@@ -121,51 +222,37 @@ contract ClaimRegistryUpgradable is
         emit Unregister(msg.sender);
     }
 
+    /**
+     * @dev Allows batch claiming for multiple withdrawal addresses.
+     * @param withdrawalAddresses Array of withdrawal addresses.
+     * @todo Consider offset shifting option for huge validators set to not get 'out of gas' error
+     */
+    function claimBatch(address[] calldata withdrawalAddresses) public {
+        for (uint256 i = 0; i < withdrawalAddresses.length; i++) {
+            claim(withdrawalAddresses[i]);
+        }
+        emit ClaimBatch(msg.sender, withdrawalAddresses);
+    }
+
+    /**
+     * @dev Claims withdrawal for a specific address and updates last claim timestamp.
+     * @param withdrawalAddress The withdrawal address to claim for.
+     */
+    function claim(address withdrawalAddress) public {
+        configs[withdrawalAddress].lastClaim = block.timestamp;
+        depositContract.claimWithdrawal(withdrawalAddress);
+    }
+
+    // Internal functions
+
+    /**
+     * @dev Sets the configuration for the caller.
+     * @param _timeThreshold Time threshold for withdrawal.
+     * @param _amountThreshold Amount threshold for withdrawal.
+     */
     function _setConfig(uint256 _timeThreshold, uint256 _amountThreshold) internal {
         configs[msg.sender].timeThreshold = _timeThreshold;
         configs[msg.sender].amountThreshold = _amountThreshold;
         configs[msg.sender].status = ConfigStatus.ACTIVE;
-    }
-
-    function isConfigActive(address _withdrawalAddress) public view returns (bool) {
-        return configs[_withdrawalAddress].status == ConfigStatus.ACTIVE;
-    }
-
-    // TODO: consider offset shifting option for huge validators set
-    function getClaimableAddresses() public view returns (address[] memory) {
-        address[] memory claimableAddresses = new address[](validators.length);
-        uint256 counter = 0;
-
-        for (uint256 i = 0; i < validators.length; i++) {
-            address val = validators[i];
-            if (depositContract.withdrawableAmount(val) == 0 || configs[val].status == ConfigStatus.INACTIVE) {
-                continue;
-            }
-            if (depositContract.withdrawableAmount(val) > configs[val].amountThreshold) {
-                claimableAddresses[counter] = val;
-                counter++;
-            } else if (
-                configs[val].timeThreshold > 0 && block.timestamp - configs[val].lastClaim > configs[val].timeThreshold
-            ) {
-                claimableAddresses[counter] = val;
-                counter++;
-            }
-        }
-
-        address[] memory trimmedClaimableAddresses = new address[](counter);
-        for (uint256 i = 0; i < claimableAddresses.length; i++) {
-            if (claimableAddresses[i] != address(0)) {
-                trimmedClaimableAddresses[i] = claimableAddresses[i];
-            }
-        }
-        return trimmedClaimableAddresses;
-    }
-
-    function resolve() public view returns (bool flag, bytes memory cdata) {
-        address[] memory addresses = getClaimableAddresses();
-        if (addresses.length == 0) {
-            return (false, "");
-        }
-        return (true, abi.encodeWithSelector(this.claimBatch.selector, addresses));
     }
 }
