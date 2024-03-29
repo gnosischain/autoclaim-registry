896ada19667491745833054c931bd98c9d6cc7b7
4rgon4ut <egorlaptev98@gmail.com>
2024-03-29 13:50:14 +0000

    chore: update deployments

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ forge doc --serve --port 4000
 
 | Name  | Gnosis Mainnet |
 | ------------- | ------------- |
-| ERC1967 Proxy  | [0xB11796Aa856762DBa6B9d42EA71d2C8D7f85a3e1](https://gnosisscan.io/address/0xB11796Aa856762DBa6B9d42EA71d2C8D7f85a3e1#code)  |
-| ClaimRegistryUpgradable (implementation) | [0xA30fee90e53FE0614995Cc31Ebb0fB0ee752B84f](https://gnosisscan.io/address/0xA30fee90e53FE0614995Cc31Ebb0fB0ee752B84f#code)  |
+| ERC1967 Proxy  | [0x8cb7C925d231f3355560F6D4D880c132B61C4083](https://gnosisscan.io/address/0x8cb7c925d231f3355560f6d4d880c132b61c4083#code)  |
+| ClaimRegistryUpgradable (implementation) | [0x89ab85199492d2bb8100fbb5f7055f5e03c83680](https://gnosisscan.io/address/0x89ab85199492d2bb8100fbb5f7055f5e03c83680#code)  |
 
 
 ## Claim automation flow
--- src/ClaimRegistryProxy.sol
+++ src/ClaimRegistryProxy.sol
@@ -1,18 +1,10 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.24;
 
 // import "./interfaces/IClaimRegistryUpgradable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967Proxy.sol"
+// import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract ClaimRegistryProxy is EIP1967Proxy {
+// contract ClaimRegistryProxy is ERC1967Proxy {
 //     constructor(IClaimRegistryUpgradable implementation, address _depositContract, uint256 _batchSizeMax) {
-//         if (implementation != address(0)) {
-//             _setImplementation(address(implementation));
-//             return;
-//         }
-//         ClaimRegistryUpgradable impl = new ClaimRegistryUpgradable();
-//         _setImplementation(address(impl));
 
-//         _delegate.initialize(_depositContract, _batchSizeMax);
-//     }
-}
+// }
